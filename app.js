663f9166efd8da4004ede57b98627134c8b3dd8e
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2023-09-26 16:56:46 +0100

    add additional endpoints

--- app.js
+++ app.js
@@ -15,9 +15,32 @@ app.get('/random/number', (req, res) => {
     res.send("Your random number is: " + random);
 })
 
-app.get('/addition', (req, res) => {
-    let random = Math.floor(Math.random() * 10000);
-    res.send("Your random number is: " + random);
+let jsonData = [
+    {
+        "alien": {
+            "name": "Xenon",
+            "origin": "Zeta Reticuli",
+            "description": "Xenon is a highly intelligent, bipedal extraterrestrial species with grayish-blue skin and large, almond-shaped black eyes. They are known for their advanced technology and telepathic communication abilities. Xenon has a deep interest in studying human culture and behavior, often conducting covert observations on Earth.",
+            "contact_status": "Indirect observation",
+            "technology_level": "Far advanced",
+            "home_planet": "Xenarion",
+            "average_lifespan": "500 Earth years",
+            "notable_traits": ["Telepathic communication", "Advanced spacecraft", "Interest in human culture"]
+        }
+    }
+];
+
+
+app.get('/json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(jsonData);
+})
+
+app.get('/addition/:num1/:num2', (req, res) => {
+    let num1 = parseInt(req.params['num1']);
+    let num2 = parseInt(req.params['num2']);
+    let answer = num1 + num2;
+    res.send("The result is: " + answer);
 })
 
 app.get('/random/message', (req, res) => {
